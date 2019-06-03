364c6d78e26c9941ace2e07e2a2550a10e8fdbf6
Sebastiaan Viaene <sebviaene@gmail.com>
2019-06-03 16:17:01 +0200

    fix: commit dist

---
+++ dist/Resolver.d.ts
@@ -0,0 +1,30 @@
+import { IFixture, IFixturesConfig } from './interface';
+export declare class Resolver {
+    private stack;
+    /**
+     * @param fixtureConfigs
+     * @return {IFixture[]}
+     */
+    resolve(fixtureConfigs: IFixturesConfig[]): IFixture[];
+    /**
+     * @param {string} parentReferenceName
+     * @param {any[] | object} propertyList
+     * @return {any[]}
+     */
+    private resolveDependencies;
+    /**
+     * @param {string} fixtureIdentify
+     * @param {string} reference
+     * @return {any}
+     */
+    private resolveReference;
+    /**
+     * @param {string} fixtureIdentify
+     */
+    private resolveCurrent;
+    /**
+     * @param item
+     * @return {any[]}
+     */
+    private resolveDeepDependencies;
+}
